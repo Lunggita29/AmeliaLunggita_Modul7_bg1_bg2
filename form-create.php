8997f939e27887fb0505286ce9c0831d4bdd0f9f
Amelia Lunggita Sapna <46413690+Lunggita29@users.noreply.github.com>
2019-03-13 12:08:03 +0700

    Add files via upload

---
+++ form-create.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html> 
+<form action="create.php" method="POST">
+<!-- form field di sini -->
+<label>Nama Dosen :</label>
+<input type="text" name="nama_dosen" />
+<br>
+<br>
+<label>No. Telepon :</label>
+<input type="text" name="telp" />
+<br>
+<br>
+
+<input type="submit" name="btnSimpan" value="Simpan" />
+
+</form>
